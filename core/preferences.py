5bc43573494ae74dc2f2e418919eb4ca2fa880a6
Alexis Ospino Gutierrez <154022491+Apepsis@users.noreply.github.com>
2026-08-23 10:55:15 -0500

    Add user preferences memory module

---
+++ core/preferences.py
@@ -0,0 +1,11 @@
+class UserPreferences:
+    """Stores explicit user preferences."""
+
+    def __init__(self):
+        self.data = {}
+
+    def set(self, key, value):
+        self.data[key] = value
+
+    def get(self, key, default=None):
+        return self.data.get(key, default)
