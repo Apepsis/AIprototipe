2a3a4c9f39e5c471552bb3c3419c94d09d2f9b8f
Alexis Ospino Gutierrez <154022491+Apepsis@users.noreply.github.com>
2026-08-23 11:12:01 -0500

    Add Windows Luna installation script

---
+++ install_luna_windows.ps1
@@ -0,0 +1,6 @@
+Write-Host "Installing Luna dependencies"
+
+python -m pip install -r requirements_voice.txt
+
+Write-Host "Dependencies installed"
+Write-Host "Next steps: download wake word and Piper voice models"
